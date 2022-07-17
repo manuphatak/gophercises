d05223ee380ea7a6309daffe2b58d4e54f9b2e5a
Manu Phatak <manu@phatak.io>
2022-07-17 10:47:59 -0500

    feat: Support yaml file via flags

--- urlshort/urlshort.go
+++ urlshort/urlshort.go
@@ -2,34 +2,68 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 
-	mux := defaultMux()
+	app := &cli.App{
+		Name:                   "urlshort",
+		Usage:                  "A url shortener",
+		Compiled:               time.Now(),
+		HideHelpCommand:        true,
+		UseShortOptionHandling: true,
+		EnableBashCompletion:   true,
+		ArgsUsage:              " ",
+		Flags: []cli.Flag{
+			&cli.StringSliceFlag{
+				Name:      "yaml",
+				Value:     cli.NewStringSlice("paths.yaml"),
+				Usage:     "A yaml file that contains the mapping of paths to urls",
+				TakesFile: true,
+				Aliases:   []string{"y"},
+			},
+		},
+		Action: func(c *cli.Context) error {
 
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
-	mapHandler := MapHandler(pathsToUrls, mux)
+			mux := defaultMux()
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-    - path: /urlshort
-      url: https://github.com/gophercises/urlshort
-    - path: /urlshort-final
-      url: https://github.com/gophercises/urlshort/tree/solution
-    `
-	yamlHandler, err := YamlHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
+			// Build the MapHandler using the mux as the fallback
+			pathsToUrls := map[string]string{
+				"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+				"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+			}
+			handler := MapHandler(pathsToUrls, mux)
+
+			yamlPaths := c.StringSlice("yaml")
+
+			for _, yamlPath := range yamlPaths {
+				yaml, err := os.ReadFile(yamlPath)
+				if err != nil {
+					return err
+				}
+
+				handler, err = YamlHandler([]byte(yaml), handler)
+				if err != nil {
+					return err
+				}
+			}
+
+			fmt.Println("Starting the server on :8080")
+			http.ListenAndServe(":8080", handler)
+			return nil
+		},
 	}
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func defaultMux() *http.ServeMux {
