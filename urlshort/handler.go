2c32eca43617efd78303b713d0dfb0cde88efcbb
Manu Phatak <manu@phatak.io>
2022-07-17 11:19:55 -0500

    feat: Support JSON file flags

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/samber/lo"
@@ -8,8 +10,13 @@ import (
 )
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	for path := range pathsToUrls {
+		fmt.Printf("Registering path: %s\n", path)
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		if redirect, ok := pathsToUrls[r.URL.Path]; ok {
+			fmt.Printf("Redirecting %s → %s\n", r.URL.Path, redirect)
 			http.Redirect(w, r, redirect, http.StatusMovedPermanently)
 		} else {
 			fallback.ServeHTTP(w, r)
@@ -17,20 +24,35 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-type PathMap struct {
+type Redirect struct {
 	Path string
 	Url  string
 }
 
-func YamlHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	paths := []PathMap{}
-
-	if err := yaml.Unmarshal(yml, &paths); err != nil {
-		return nil, err
-	}
-	pathsToUrls := lo.FromEntries(lo.Map(paths, func(path PathMap, _ int) lo.Entry[string, string] {
+func toPathMap(paths []Redirect) map[string]string {
+	return lo.FromEntries(lo.Map(paths, func(path Redirect, _ int) lo.Entry[string, string] {
 		return lo.Entry[string, string]{Key: path.Path, Value: path.Url}
 	}))
+}
+
+func YamlHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	redirects := []Redirect{}
+
+	if err := yaml.Unmarshal(yml, &redirects); err != nil {
+		return nil, err
+	}
+	pathsToUrls := toPathMap(redirects)
+
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func JsonHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	redirects := []Redirect{}
+
+	if err := json.Unmarshal(yml, &redirects); err != nil {
+		return nil, err
+	}
+	pathsToUrls := toPathMap(redirects)
 
 	return MapHandler(pathsToUrls, fallback), nil
 }
--- urlshort/urlshort.go
+++ urlshort/urlshort.go
@@ -24,34 +24,32 @@ func main() {
 			&cli.StringSliceFlag{
 				Name:      "yaml",
 				Value:     cli.NewStringSlice("paths.yaml"),
-				Usage:     "A yaml file that contains the mapping of paths to urls",
+				Usage:     "A YAML file that contains the mapping of paths to urls",
 				TakesFile: true,
 				Aliases:   []string{"y"},
 			},
+			&cli.StringSliceFlag{
+				Name:      "json",
+				Value:     cli.NewStringSlice(),
+				Usage:     "A JSON file that contains the mapping of paths to urls",
+				TakesFile: true,
+				Aliases:   []string{"j"},
+			},
 		},
 		Action: func(c *cli.Context) error {
-
-			mux := defaultMux()
-
-			// Build the MapHandler using the mux as the fallback
-			pathsToUrls := map[string]string{
-				"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-				"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-			}
-			handler := MapHandler(pathsToUrls, mux)
+			var err error
+			handler := defaultHandler()
 
 			yamlPaths := c.StringSlice("yaml")
+			handler, err = loadYamlRedirects(yamlPaths, handler)
+			if err != nil {
+				return err
+			}
 
-			for _, yamlPath := range yamlPaths {
-				yaml, err := os.ReadFile(yamlPath)
-				if err != nil {
-					return err
-				}
-
-				handler, err = YamlHandler([]byte(yaml), handler)
-				if err != nil {
-					return err
-				}
+			jsonPaths := c.StringSlice("json")
+			handler, err = loadJsonRedirects(jsonPaths, handler)
+			if err != nil {
+				return err
 			}
 
 			fmt.Println("Starting the server on :8080")
@@ -72,4 +70,45 @@ func defaultMux() *http.ServeMux {
 		fmt.Fprintln(w, "Hello, world!")
 	})
 	return mux
+}
+
+func defaultHandler() http.HandlerFunc {
+	mux := defaultMux()
+
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	// Build the MapHandler using the mux as the fallback
+	return MapHandler(pathsToUrls, mux)
+}
+
+func loadYamlRedirects(yamlPaths []string, handler http.HandlerFunc) (http.HandlerFunc, error) {
+	for _, yamlPath := range yamlPaths {
+		yaml, err := os.ReadFile(yamlPath)
+		if err != nil {
+			return nil, err
+		}
+
+		handler, err = YamlHandler([]byte(yaml), handler)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return handler, nil
+}
+
+func loadJsonRedirects(jsonPaths []string, handler http.HandlerFunc) (http.HandlerFunc, error) {
+	for _, jsonPath := range jsonPaths {
+		json, err := os.ReadFile(jsonPath)
+		if err != nil {
+			return nil, err
+		}
+
+		handler, err = JsonHandler([]byte(json), handler)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return handler, nil
 }
